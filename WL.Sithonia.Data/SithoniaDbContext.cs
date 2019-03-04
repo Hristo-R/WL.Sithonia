ba2456be7e067813acc636e29289d51f9767b566
Hristo-R <hristo.razpopov@abv.bg>
2019-03-04 05:51:38 +0200

    Added folder Data

---
+++ WL.Sithonia.Data/SithoniaDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace WL.Sithonia.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using WL.Sithonia.Models;
+
+    public class SithoniaDbContext : IdentityDbContext<User>
+    {
+        public SithoniaDbContext(DbContextOptions<SithoniaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}

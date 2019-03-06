4dfa6a2098cd500c2106866fd7ce850cdbc9cb4a
Hristo-R <hristo.razpopov@abv.bg>
2019-03-06 18:21:24 +0200

    Added Controllers/SithoniaController.cs

--- WL.Sithonia.Web/Controllers/SithoniaController.cs
+++ WL.Sithonia.Web/Controllers/SithoniaController.cs
@@ -3,6 +3,8 @@
     using Microsoft.AspNetCore.Mvc;
     using System.Linq;
     using WL.Sithonia.Data;
+    using WL.Sithonia.Models;
+    using WL.Sithonia.Web.Models.BindingModels;
     using WL.Sithonia.Web.Models.ViewModels;
 
     public class SithoniaController : Controller
@@ -26,9 +28,24 @@
                 }).ToList();
 
             //Add a Dummy Row.
-            //customers.Insert(0, new CustomerViewModel());
+            customers.Insert(0, new CustomerViewModel());
 
             return this.View(customers);
+        }
+
+        [HttpPost]
+        public JsonResult InsertCustomer(CustomerInsertModel customer)
+        {
+            var newCustomer = new Customer
+            {
+                Name = customer.Name,
+                Country = customer.Country
+            };
+
+            this.db.Customers.Add(newCustomer);
+            this.db.SaveChanges();
+
+            return Json(customer);
         }
     }
 }
---
+++ WL.Sithonia.Web/Models/BindingModels/CustomerInsertModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WL.Sithonia.Web.Models.BindingModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CustomerInsertModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Country { get; set; }
+    }
+}
